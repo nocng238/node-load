5c769f95b1ef31ca519ff8302827177cb6f31fa1
=nogc238 <=nmtngoc238@gmail.com>
2024-04-14 22:41:52 +0700

    add docker compose file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT;
+  await app.listen(port);
+  console.log(`App starting on port ${port}`);
 }
 bootstrap();
