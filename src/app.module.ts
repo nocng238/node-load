b7ae49abf0311f4bdc8b718b8bd1535b6c0fe2d2
=nogc238 <=nmtngoc238@gmail.com>
2024-04-15 22:41:16 +0700

    added db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MediaController } from './media/media.controller';
+import { MediaModule } from './media/media.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// import { Media } from './entity/medias.entity';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          database: configService.get('DB_DATABASE'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          entities: [],
+          synchronize: true,
+          keepConnectionAlive: true,
+        };
+      },
+    }),
+    MediaModule,
+  ],
+  controllers: [AppController, MediaController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/entity/medias.entity.ts
@@ -0,0 +1,4 @@
+import { Entity } from 'typeorm';
+
+@Entity('medias')
+export class Media {}
